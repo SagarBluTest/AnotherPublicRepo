4ba6f56aa53b562eb4d151dd045628cf9316e60d
Sagar Potluru <sagar@aptuz.com>
2022-11-09 20:06:52 +0530

    added new file

---
+++ src.py
@@ -0,0 +1,11 @@
+password = "Pass12345!!XXabc"
+credentials = "OpenF3ast123@"
+
+
+class db:
+  def __init__(self, connection):
+    self._connection = connection
+    
+  def connect_to_db(self):
+    self._connection.connect(user=credentials, password=password)
+    password = "Micro@12345!!XXabc"
